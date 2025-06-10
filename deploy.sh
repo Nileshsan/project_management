92d78b39d9d36c7262f921975485ebd7bccd2838
Nilesh <nileshsanyasi02@gmail.com>
2025-06-10 16:54:47 +0530

    cache clear

--- deploy.sh
+++ deploy.sh
@@ -33,8 +33,11 @@ php artisan key:generate --force
 # Clear any previous cached data
 php artisan config:clear
 php artisan cache:clear
-php artisan view:clear
 php artisan route:clear
+php artisan view:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
 # Set up storage link
 php artisan storage:link --force
