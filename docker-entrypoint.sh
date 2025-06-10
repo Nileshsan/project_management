d7af1e8dc165e2d503900179708ec96f9e747acd
Nilesh <nileshsanyasi02@gmail.com>
2025-06-10 16:49:58 +0530

    changes for error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,21 +11,20 @@ cp /var/www/nileshenv /var/www/.env || echo "Failed to copy nileshenv to .env"
 if [ -n "$APP_KEY" ]; then
     sed -i "s#APP_KEY=.*#APP_KEY=$APP_KEY#g" /var/www/.env
 fi
-    if [ -n "$APP_URL" ]; then
-        sed -i "s#APP_URL=.*#APP_URL=$APP_URL#g" /var/www/.env
-    fi
-    if [ -n "$DB_HOST" ]; then
-        sed -i "s#DB_HOST=.*#DB_HOST=$DB_HOST#g" /var/www/.env
-    fi
-    if [ -n "$DB_DATABASE" ]; then
-        sed -i "s#DB_DATABASE=.*#DB_DATABASE=$DB_DATABASE#g" /var/www/.env
-    fi
-    if [ -n "$DB_USERNAME" ]; then
-        sed -i "s#DB_USERNAME=.*#DB_USERNAME=$DB_USERNAME#g" /var/www/.env
-    fi
-    if [ -n "$DB_PASSWORD" ]; then
-        sed -i "s#DB_PASSWORD=.*#DB_PASSWORD=$DB_PASSWORD#g" /var/www/.env
-    fi
+if [ -n "$APP_URL" ]; then
+    sed -i "s#APP_URL=.*#APP_URL=$APP_URL#g" /var/www/.env
+fi
+if [ -n "$DB_HOST" ]; then
+    sed -i "s#DB_HOST=.*#DB_HOST=$DB_HOST#g" /var/www/.env
+fi
+if [ -n "$DB_DATABASE" ]; then
+    sed -i "s#DB_DATABASE=.*#DB_DATABASE=$DB_DATABASE#g" /var/www/.env
+fi
+if [ -n "$DB_USERNAME" ]; then
+    sed -i "s#DB_USERNAME=.*#DB_USERNAME=$DB_USERNAME#g" /var/www/.env
+fi
+if [ -n "$DB_PASSWORD" ]; then
+    sed -i "s#DB_PASSWORD=.*#DB_PASSWORD=$DB_PASSWORD#g" /var/www/.env
 fi
 
 # Ensure directories exist and have correct permissions
